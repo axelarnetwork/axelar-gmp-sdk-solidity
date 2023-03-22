22fb7bd214dc55d005469c9f957861b8af32ac10
Kiryl Yermakou <rma4ok@gmail.com>
2023-03-21 20:14:26 -0400

    feature(ExpressExecutable): acceptExpressCallWithToken

--- contracts/interfaces/IExpressExecutable.sol
+++ contracts/interfaces/IExpressExecutable.sol
@@ -5,5 +5,12 @@ pragma solidity ^0.8.0;
 import { IAxelarExecutable } from './IAxelarExecutable.sol';
 
 interface IExpressExecutable is IAxelarExecutable {
-    function enableExpressCallWithToken() external returns (bool);
+    function acceptExpressCallWithToken(
+        address caller, /*caller*/
+        string calldata sourceChain, /*sourceChain*/
+        string calldata sourceAddress, /*sourceAddress*/
+        bytes32 payloadHash, /*payloadHash*/
+        string calldata tokenSymbol, /*tokenSymbol*/
+        uint256 /*amount*/
+    ) external returns (bool);
 }
--- contracts/interfaces/IExpressProxy.sol
+++ contracts/interfaces/IExpressProxy.sol
@@ -8,7 +8,7 @@ import { IExpressRegistry } from './IExpressRegistry.sol';
 interface IExpressProxy is IAxelarExecutable {
     error NotExpressRegistry();
     error InvalidTokenSymbol();
-    error ExpressCallNotEnabled();
+    error ExpressCallNotAccepted();
 
     function registry() external view returns (IExpressRegistry);
 

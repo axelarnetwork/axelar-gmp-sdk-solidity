f773ed280e72eeab00b61e46bc063cc4fb40ecbd
Milap Sheth <milap@axelar.network>
2023-04-24 16:26:31 +0530

    remove unused import

--- contracts/utils/Ownable.sol
+++ contracts/utils/Ownable.sol
@@ -2,8 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-import '../interfaces/IUpgradable.sol';
-
 abstract contract Ownable is IOwnable {
     // keccak256('owner')
     bytes32 internal constant _OWNER_SLOT = 0x02016836a56b71f0d02689e69e326f4f4c1b9057164ef592671cf0d37c8040c0;

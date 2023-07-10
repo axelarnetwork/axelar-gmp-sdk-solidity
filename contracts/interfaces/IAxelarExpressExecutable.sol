bfc6972a19e0848a0bb20188b1b06cced550e529
Foivos <foivos@umich.edu>
2023-07-10 17:52:11 +0300

    added natspec

--- contracts/interfaces/IAxelarExpressExecutable.sol
+++ contracts/interfaces/IAxelarExpressExecutable.sol
@@ -4,11 +4,24 @@ pragma solidity ^0.8.0;
 
 import { IAxelarExecutable } from './IAxelarExecutable.sol';
 
+/**
+ * @title IAxelarExpressExecutable
+ * @notice Interface for the Axelar Express Executable contract.
+ */
 interface IAxelarExpressExecutable is IAxelarExecutable {
+    // Custom errors
     error AlreadyExecuted();
     error InsufficientValue();
     error ExpressExecutorAlreadySet();
 
+    /**
+     * @notice Emitted when an express execution is successfully performed.
+     * @param commandId The unique identifier for the command.
+     * @param sourceChain The source chain.
+     * @param sourceAddress The source address.
+     * @param payloadHash The hash of the payload.
+     * @param expressExecutor The address of the express executor.
+     */
     event ExpressExecuted(
         bytes32 indexed commandId,
         string sourceChain,
@@ -17,6 +30,16 @@ interface IAxelarExpressExecutable is IAxelarExecutable {
         address indexed expressExecutor
     );
 
+    /**
+     * @notice Emitted when an express execution with a token is successfully performed.
+     * @param commandId The unique identifier for the command.
+     * @param sourceChain The source chain.
+     * @param sourceAddress The source address.
+     * @param payloadHash The hash of the payload.
+     * @param symbol The token symbol.
+     * @param amount The amount of tokens.
+     * @param expressExecutor The address of the express executor.
+     */
     event ExpressExecutedWithToken(
         bytes32 indexed commandId,
         string sourceChain,
@@ -27,6 +50,14 @@ interface IAxelarExpressExecutable is IAxelarExecutable {
         address indexed expressExecutor
     );
 
+    /**
+     * @notice Emitted when an express execution is fulfilled.
+     * @param commandId The commandId for the contractCall.
+     * @param sourceChain The source chain.
+     * @param sourceAddress The source address.
+     * @param payloadHash The hash of the payload.
+     * @param expressExecutor The address of the express executor.
+     */
     event ExpressExecutionFulfilled(
         bytes32 indexed commandId,
         string sourceChain,
@@ -35,6 +66,16 @@ interface IAxelarExpressExecutable is IAxelarExecutable {
         address indexed expressExecutor
     );
 
+    /**
+     * @notice Emitted when an express execution with a token is fulfilled.
+     * @param commandId The commandId for the contractCallWithToken.
+     * @param sourceChain The source chain.
+     * @param sourceAddress The source address.
+     * @param payloadHash The hash of the payload.
+     * @param symbol The token symbol.
+     * @param amount The amount of tokens.
+     * @param expressExecutor The address of the express executor.
+     */
     event ExpressExecutionWithTokenFulfilled(
         bytes32 indexed commandId,
         string sourceChain,
@@ -45,6 +86,14 @@ interface IAxelarExpressExecutable is IAxelarExecutable {
         address indexed expressExecutor
     );
 
+    /**
+     * @notice Returns the express executor for a given command.
+     * @param commandId The commandId for the contractCall.
+     * @param sourceChain The source chain.
+     * @param sourceAddress The source address.
+     * @param payloadHash The hash of the payload.
+     * @return expressExecutor The address of the express executor.
+     */
     function getExpressExecutor(
         bytes32 commandId,
         string calldata sourceChain,
@@ -52,6 +101,16 @@ interface IAxelarExpressExecutable is IAxelarExecutable {
         bytes32 payloadHash
     ) external view returns (address expressExecutor);
 
+    /**
+     * @notice Returns the express executor with token for a given command.
+     * @param commandId The commandId for the contractCallWithToken.
+     * @param sourceChain The source chain.
+     * @param sourceAddress The source address.
+     * @param payloadHash The hash of the payload.
+     * @param symbol The token symbol.
+     * @param amount The amount of tokens.
+     * @return expressExecutor The address of the express executor.
+     */
     function getExpressExecutorWithToken(
         bytes32 commandId,
         string calldata sourceChain,
@@ -61,6 +120,13 @@ interface IAxelarExpressExecutable is IAxelarExecutable {
         uint256 amount
     ) external view returns (address expressExecutor);
 
+    /**
+     * @notice Express executes a contract call.
+     * @param commandId The commandId for the contractCall.
+     * @param sourceChain The source chain.
+     * @param sourceAddress The source address.
+     * @param payload The payload data.
+     */
     function expressExecute(
         bytes32 commandId,
         string calldata sourceChain,
@@ -68,6 +134,15 @@ interface IAxelarExpressExecutable is IAxelarExecutable {
         bytes calldata payload
     ) external payable;
 
+    /**
+     * @notice Express executes a contract call with token.
+     * @param commandId The commandId for the contractCallWithToken.
+     * @param sourceChain The source chain.
+     * @param sourceAddress The source address.
+     * @param payload The payload data.
+     * @param symbol The token symbol.
+     * @param amount The amount of token.
+     */
     function expressExecuteWithToken(
         bytes32 commandId,
         string calldata sourceChain,

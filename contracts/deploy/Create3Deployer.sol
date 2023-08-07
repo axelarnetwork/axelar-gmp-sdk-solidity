2c14aa877d11340a275262fa6204f1d0957b6777
Dean Amiel <dean@axelar.network>
2023-08-06 23:13:26 -0400

    update deployer files and add interfaces

--- contracts/deploy/Create3Deployer.sol
+++ contracts/deploy/Create3Deployer.sol
@@ -2,21 +2,18 @@
 
 pragma solidity ^0.8.0;
 
+import { ICreate3Deployer } from '../interfaces/ICreate3Deployer.sol';
 import { Create3 } from './Create3.sol';
 
 /**
  * @title Create3Deployer Contract
- * @notice This contract is responsible for deploying and initializing new contracts using the CREATE3 technique
+ * @notice This contract is responsible for deploying and initializing new contracts using the `CREATE3` technique
  * which ensures that only the sender address and salt influence the deployed address, not the contract bytecode.
  */
-contract Create3Deployer {
-    error FailedInit();
-
-    event Deployed(bytes32 indexed bytecodeHash, bytes32 indexed salt, address indexed deployedAddress);
-
+contract Create3Deployer is ICreate3Deployer {
     /**
-     * @dev Deploys a contract using `CREATE3`. The address where the contract
-     * will be deployed can be known in advance via {deployedAddress}.
+     * @notice Deploys a contract using `CREATE3`.
+     * @dev The address where the contract will be deployed can be known in advance via {deployedAddress}.
      *
      * The bytecode for a contract can be obtained from Solidity with
      * `type(contractName).creationCode`.
@@ -25,6 +22,10 @@ contract Create3Deployer {
      *
      * - `bytecode` must not be empty.
      * - `salt` must not have been used already by the same `msg.sender`.
+     *
+     * @param bytecode The bytecode of the contract to be deployed
+     * @param salt A salt to further randomize the contract address
+     * @return deployedAddress_ The address of the deployed contract
      */
     function deploy(bytes calldata bytecode, bytes32 salt) external returns (address deployedAddress_) {
         bytes32 deploySalt = keccak256(abi.encode(msg.sender, salt));
@@ -34,8 +35,8 @@ contract Create3Deployer {
     }
 
     /**
-     * @dev Deploys a contract using `CREATE3` and initialize it. The address where the contract
-     * will be deployed can be known in advance via {deployedAddress}.
+     * @notice Deploys a contract using `CREATE3` and initialize it.
+     * @dev The address where the contract will be deployed can be known in advance via {deployedAddress}.
      *
      * The bytecode for a contract can be obtained from Solidity with
      * `type(contractName).creationCode`.
@@ -45,6 +46,11 @@ contract Create3Deployer {
      * - `bytecode` must not be empty.
      * - `salt` must not have been used already by the same `msg.sender`.
      * - `init` is used to initialize the deployed contract
+     *
+     * @param bytecode The bytecode of the contract to be deployed
+     * @param salt A salt to further randomize the contract address
+     * @param init Init data used to initialize the deployed contract
+     * @return deployedAddress_ The address of the deployed contract
      */
     function deployAndInit(
         bytes memory bytecode,
@@ -61,8 +67,11 @@ contract Create3Deployer {
     }
 
     /**
-     * @dev Returns the address where a contract will be stored if deployed via {deploy} or {deployAndInit} by `sender`.
-     * Any change in `sender` or `salt` will result in a new destination address.
+     * @notice Returns the address where a contract will be stored if deployed via {deploy} or {deployAndInit} by `sender`.
+     * @dev Any change in `sender` or `salt` will result in a new destination address.
+     * @param sender The address that will deploy the contract via `CREATE3`
+     * @param salt The salt that will be used to further randomize the contract address
+     * @return address The address that the contract will be deployed to using `CREATE3`
      */
     function deployedAddress(address sender, bytes32 salt) external view returns (address) {
         bytes32 deploySalt = keccak256(abi.encode(sender, salt));

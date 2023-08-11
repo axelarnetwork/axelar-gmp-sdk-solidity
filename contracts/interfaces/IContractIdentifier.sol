21c8f2a83e1977e608c913bd269f9bedf2def0b7
Kiryl Yermakou <rma4ok@gmail.com>
2023-08-11 19:43:40 -0400

    Update contracts/interfaces/IContractIdentifier.sol
    Co-authored-by: Milap Sheth <milap@axelar.network>

--- contracts/interfaces/IContractIdentifier.sol
+++ contracts/interfaces/IContractIdentifier.sol
@@ -5,8 +5,8 @@ pragma solidity ^0.8.0;
 // General interface for upgradable contracts
 interface IContractIdentifier {
     /**
-     * @dev Returns the contract ID. It can be used as a check during upgrades.
-     * @notice Meant to be overridden in derived contracts.
+     * @notice Returns the contract ID. It can be used as a check during upgrades.
+     * @dev Meant to be overridden in derived contracts.
      * @return bytes32 The contract ID
      */
     function contractId() external pure returns (bytes32);

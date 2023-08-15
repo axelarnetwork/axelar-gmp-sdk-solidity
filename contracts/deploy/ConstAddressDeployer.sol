92e5a36973a0ac3f7f4910263d431223e09764f0
Dean Amiel <dean@axelar.network>
2023-08-15 02:15:11 -0400

    feat: address comments and add NatSpec

--- contracts/deploy/ConstAddressDeployer.sol
+++ contracts/deploy/ConstAddressDeployer.sol
@@ -2,16 +2,18 @@
 
 pragma solidity ^0.8.0;
 
-contract ConstAddressDeployer {
-    error EmptyBytecode();
-    error FailedDeploy();
-    error FailedInit();
+import { ICreate2Deployer } from '../interfaces/ICreate2Deployer.sol';
 
-    event Deployed(bytes32 indexed bytecodeHash, bytes32 indexed salt, address indexed deployedAddress);
-
+/**
+ * @title ConstAddressDeployer Contract
+ * @notice This contract is responsible for deploying and initializing new contracts using the `CREATE2` method
+ * which computes the deployed contract address based on the bytecode, deployer address, and deployment salt.
+ */
+contract ConstAddressDeployer is ICreate2Deployer {
     /**
-     * @dev Deploys a contract using `CREATE2`. The address where the contract
-     * will be deployed can be known in advance via {deployedAddress}.
+     * @notice Deploys a contract using `CREATE2`.
+     * @dev The address where the contract will be deployed can be known in
+     * advance via {deployedAddress}.
      *
      * The bytecode for a contract can be obtained from Solidity with
      * `type(contractName).creationCode`.
@@ -20,14 +22,19 @@ contract ConstAddressDeployer {
      *
      * - `bytecode` must not be empty.
      * - `salt` must have not been used for `bytecode` already by the same `msg.sender`.
+     *
+     * @param bytecode The bytecode of the contract to be deployed
+     * @param salt A salt to further randomize the contract address
+     * @return deployedAddress_ The address of the deployed contract
      */
     function deploy(bytes memory bytecode, bytes32 salt) external returns (address deployedAddress_) {
         deployedAddress_ = _deploy(bytecode, keccak256(abi.encode(msg.sender, salt)));
     }
 
     /**
-     * @dev Deploys a contract using `CREATE2` and initialize it. The address where the contract
-     * will be deployed can be known in advance via {deployedAddress}.
+     * @notice Deploys a contract using `CREATE2` and initialize it.
+     * @dev The address where the contract will be deployed can be known in advance
+     * via {deployedAddress}.
      *
      * The bytecode for a contract can be obtained from Solidity with
      * `type(contractName).creationCode`.
@@ -38,6 +45,11 @@ contract ConstAddressDeployer {
      * - `salt` must have not been used for `bytecode` already by the same `msg.sender`.
      * - `init` is used to initialize the deployed contract
      *    as an option to not have the constructor args affect the address derived by `CREATE2`.
+     *
+     * @param bytecode The bytecode of the contract to be deployed
+     * @param salt A salt to further randomize the contract address
+     * @param init Init data used to initialize the deployed contract
+     * @return deployedAddress_ The address of the deployed contract
      */
     function deployAndInit(
         bytes memory bytecode,
@@ -52,8 +64,12 @@ contract ConstAddressDeployer {
     }
 
     /**
-     * @dev Returns the address where a contract will be stored if deployed via {deploy} or {deployAndInit} by `sender`.
-     * Any change in the `bytecode`, `sender`, or `salt` will result in a new destination address.
+     * @notice Returns the address where a contract will be stored if deployed via {deploy} or {deployAndInit} by `sender`.
+     * @dev Any change in the `bytecode`, `sender`, or `salt` will result in a new destination address.
+     * @param bytecode The bytecode of the contract to be deployed
+     * @param sender The address that will deploy the contract via `CREATE2`
+     * @param salt The salt that will be used to further randomize the contract address
+     * @return deployedAddress_ The address that the contract will be deployed to using `CREATE2`
      */
     function deployedAddress(
         bytes calldata bytecode,
@@ -77,6 +93,10 @@ contract ConstAddressDeployer {
         );
     }
 
+    /**
+     * @dev Internal function that deploys a contract with the provided bytecode and deployment salt using
+     * the `CREATE2` opcode.
+     */
     function _deploy(bytes memory bytecode, bytes32 salt) internal returns (address deployedAddress_) {
         if (bytecode.length == 0) revert EmptyBytecode();
 

11a8d6ca6c274f0aee56d80be23ca7dba131547e
Kiryl Yermakou <rma4ok@gmail.com>
2023-08-21 18:15:36 -0400

    Update contracts/upgradable/FinalProxy.sol
    Co-authored-by: Milap Sheth <milap@axelar.network>

--- contracts/upgradable/FinalProxy.sol
+++ contracts/upgradable/FinalProxy.sol
@@ -16,7 +16,7 @@ import { Proxy } from './Proxy.sol';
  * the IFinalProxy interface.
  */
 contract FinalProxy is Create3, Proxy, IFinalProxy {
-    // FINAL_IMPLEMENTATION_SALT = bytes32(uint256(keccak256('final-implementation')) - 1);
+    // bytes32(uint256(keccak256('final-implementation')) - 1);
     bytes32 internal constant FINAL_IMPLEMENTATION_SALT =
         0x80df4dfef2d6527a47431f6f203697684e26d83f81418443821420778d4c4e8c;
 

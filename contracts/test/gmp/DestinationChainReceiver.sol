9080a874fa2f91131a987fa1b1b843e8e17cecc6
Dean <deanamiel1@gmail.com>
2023-09-11 15:19:10 -0400

    feat: full test coverage (#85)
    * feat: unit test coverage
    
    * fix: slither
    
    ---------
    
    Co-authored-by: Dean Amiel <dean@axelar.network>

---
+++ contracts/test/gmp/DestinationChainReceiver.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import { AxelarExecutable } from '../../executable/AxelarExecutable.sol';
+
+contract DestinationChainReceiver is AxelarExecutable {
+    event Received(uint256 num);
+
+    constructor(address gatewayAddress) AxelarExecutable(gatewayAddress) {}
+
+    function _execute(
+        string calldata, /*sourceChain*/
+        string calldata, /*sourceAddress*/
+        bytes calldata payload
+    ) internal override {
+        uint256 num = abi.decode(payload, (uint256));
+        emit Received(num);
+    }
+}

98340440c692e43416d364befac1450cd0bd6174
Dean Amiel <dean@axelar.network>
2023-09-11 01:18:40 -0400

    feat: unit test coverage

--- contracts/test/upgradable/TestFinalProxy.sol
+++ contracts/test/upgradable/TestFinalProxy.sol
@@ -13,4 +13,8 @@ contract TestFinalProxy is FinalProxy {
         if (FINAL_IMPLEMENTATION_SALT != bytes32(uint256(keccak256('final-implementation')) - 1))
             revert('invalid final salt');
     }
+
+    function contractId() internal pure override returns (bytes32) {
+        return keccak256('proxy-implementation');
+    }
 }
--- contracts/test/upgradable/UpgradableTest.sol
+++ contracts/test/upgradable/UpgradableTest.sol
@@ -5,10 +5,16 @@ pragma solidity ^0.8.0;
 import { Upgradable } from '../../upgradable/Upgradable.sol';
 
 contract UpgradableTest is Upgradable {
+    uint256 public num;
+
     constructor() Upgradable() {
         if (_IMPLEMENTATION_SLOT != bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1)) {
             revert('invalid implementation slot');
         }
+    }
+
+    function _setup(bytes calldata data) internal override {
+        num = abi.decode(data, (uint256));
     }
 
     function contractId() external pure override returns (bytes32) {

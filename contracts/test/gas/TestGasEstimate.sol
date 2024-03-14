62a07f42ea473ee10b9084646adc7276a0dd3684
Kiryl Yermakou <rma4ok@gmail.com>
2024-03-13 21:09:40 -0400

    test(GasEstimate): adding test contract

--- contracts/test/gas/TestGasEstimate.sol
+++ contracts/test/gas/TestGasEstimate.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import { GasEstimate } from '../../gas/GasEstimate.sol';
 
 contract TestGasEstimate is GasEstimate {
-    function updateGasInfo(string calldata chain, GasInfo info) external {
+    function updateGasInfo(string calldata chain, GasInfo calldata info) external {
         _setGasInfo(chain, info);
     }
 }

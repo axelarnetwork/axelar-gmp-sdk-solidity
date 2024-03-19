21b10cae2dbcf2fcf6707e082ba1885970cef745
Kiryl Yermakou <rma4ok@gmail.com>
2024-03-18 23:23:03 -0400

    fix(InterchainMultisig): storage slot location

--- contracts/test/governance/TestBaseWeightedMultisig.sol
+++ contracts/test/governance/TestBaseWeightedMultisig.sol
@@ -6,7 +6,7 @@ import { BaseWeightedMultisig } from '../../governance/BaseWeightedMultisig.sol'
 
 contract TestBaseWeightedMultisig is BaseWeightedMultisig {
     constructor(uint256 oldSignersRetention) BaseWeightedMultisig(oldSignersRetention) {
-        if (BASE_WEIGHTED_MULTISIG_SLOT != keccak256('BaseWeightedMultisig.Slot')) {
+        if (BASE_WEIGHTED_MULTISIG_SLOT != bytes32(uint256(keccak256('BaseWeightedMultisig.Slot')) - 1)) {
             revert('WeightedMultisig.Slot');
         }
     }
--- contracts/test/governance/TestInterchainMultisig.sol
+++ contracts/test/governance/TestInterchainMultisig.sol
@@ -8,7 +8,7 @@ contract TestInterchainMultisig is InterchainMultisig {
     constructor(string memory chainName, WeightedSigners memory weightedSigners)
         InterchainMultisig(chainName, weightedSigners)
     {
-        if (INTERCHAIN_MULTISIG_SLOT != keccak256('InterchainMultisig.Slot')) {
+        if (INTERCHAIN_MULTISIG_SLOT != bytes32(uint256(keccak256('InterchainMultisig.Slot')) - 1)) {
             revert('InterchainMultisig.Slot');
         }
     }

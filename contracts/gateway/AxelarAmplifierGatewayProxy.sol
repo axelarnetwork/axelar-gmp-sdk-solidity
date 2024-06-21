3a9cf096097cdaf18c9a9b61ae9ce0b33516b98c
blockchainguyy <ayusht11@outlook.com>
2024-06-21 19:48:30 +0530

    chore: add contractId in AxelarAmplifierGatewayProxy

--- contracts/gateway/AxelarAmplifierGatewayProxy.sol
+++ contracts/gateway/AxelarAmplifierGatewayProxy.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.8.0;
 import { Proxy } from '../upgradable/Proxy.sol';
 
 contract AxelarAmplifierGatewayProxy is Proxy {
+    bytes32 private constant CONTRACT_ID = keccak256('axelar-amplifier-gateway-proxy');
+
     constructor(
         address implementationAddress,
         address owner,
@@ -13,5 +15,9 @@ contract AxelarAmplifierGatewayProxy is Proxy {
 
     function contractId() internal pure override returns (bytes32) {
         return keccak256('axelar-amplifier-gateway');
+    }
+
+    function getContractId() public pure returns (bytes32) {
+        return CONTRACT_ID;
     }
 }

9c465f67777f3b4b37aabd05edd39b7a0e1068b0
blockchainguyy <ayusht11@outlook.com>
2024-06-24 17:06:58 +0530

    chore: make contractId constant public

--- contracts/gateway/AxelarAmplifierGatewayProxy.sol
+++ contracts/gateway/AxelarAmplifierGatewayProxy.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import { Proxy } from '../upgradable/Proxy.sol';
 
 contract AxelarAmplifierGatewayProxy is Proxy {
-    bytes32 private constant CONTRACT_ID = keccak256('axelar-amplifier-gateway-proxy');
+    bytes32 public constant CONTRACT_ID = keccak256('axelar-amplifier-gateway');
 
     constructor(
         address implementationAddress,
@@ -14,10 +14,6 @@ contract AxelarAmplifierGatewayProxy is Proxy {
     ) Proxy(implementationAddress, owner, setupParams) {}
 
     function contractId() internal pure override returns (bytes32) {
-        return keccak256('axelar-amplifier-gateway');
-    }
-
-    function getContractId() public pure returns (bytes32) {
         return CONTRACT_ID;
     }
 }

586d3fdb0008c820b27cf830ce5ef006711285da
SGiaccobasso <sgiaccobasso@gmail.com>
2024-09-20 14:32:33 -0300

    chore: add transfer received event

--- contracts/interfaces/IAxelarGMPTransfer.sol
+++ contracts/interfaces/IAxelarGMPTransfer.sol
@@ -3,31 +3,49 @@
 pragma solidity ^0.8.0;
 
 /**
- * @title IAxelarGMPTransfer
+ * @title IInterchainTransferSent
  * @dev Interface for tracking asset value transfers using General Message Passing (GMP) calls in the Axelar Network.
- * This interface defines an event that should be emitted when a GMP transfer occurs,
+ * This interface defines an event that should be emitted when a GMP transfer is sent,
  * allowing for standardized volume tracking across different implementations.
  */
-interface IAxelarGMPTransfer {
+interface IInterchainTransferSent {
     /**
-     * @dev Emitted when a GMP transfer occurs, providing details for volume tracking.
-     * @param sender The address of the caller that initiated the transfer on the source chain.
-     * @param sourceChain The Axelar chain identifier of the source chain.
+     * @dev Emitted when a GMP transfer is sent, providing details for volume tracking.
      * @param destinationChain The Axelar chain identifier of the destination chain.
-     * @param destinationAddress The address of the contract on the destination chain that receives the transfer.
-     * @param recipientAddress The address of the final recipient of the transferred assets on the destination chain.
-     * @param tokenAddress The address of the token contract on the source chain.
+     * @param destinationContractAddress The address of the contract on the destination chain that receives the transfer.
+     * @param recipient The address of the final recipient of the transferred assets on the destination chain.
+     * @param token The address of the token contract on the source chain.
      * @param amount The amount (in atomic units) of tokens transferred.
-     * @param decimals The number of decimal places for the token.
      */
-    event AxelarGMPTransfer(
-        address indexed sender,
-        string sourceChain,
+    event InterchainTransferSent(
         string destinationChain,
         string destinationContractAddress,
         bytes recipient,
         address token,
         uint256 amount,
-        uint256 decimals
+    );
+}
+
+/**
+ * @title IInterchainTransferReceived
+ * @dev Interface for tracking asset value transfers using General Message Passing (GMP) calls in the Axelar Network.
+ * This interface defines an event that should be emitted when a GMP transfer is received,
+ * allowing for standardized volume tracking across different implementations.
+ */
+interface IInterchainTransferReceived {
+    /**
+     * @dev Emitted when an interchain transfer is received, providing details for volume tracking.
+     * @param sender The address of the caller that initiated the transfer on the source chain.
+     * @param sourceChain The Axelar chain identifier of the source chain.
+     * @param recipient The address of the final recipient of the transferred assets on the destination chain.
+     * @param token The address of the token contract on the destination chain.
+     * @param amount The amount (in atomic units) of tokens received.
+     */
+    event InterchainTransferReceived(
+        string sender,
+        string sourceChain,
+        bytes recipient,
+        address token,
+        uint256 amount,
     );
 }

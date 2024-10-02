6ad4eb2d693d4c99b931afe7739a76fc2f552f7a
npty <78221556+npty@users.noreply.github.com>
2024-10-02 19:09:44 +0700

    chore: indexed sender
    Co-authored-by: Milap Sheth <milap@interoplabs.io>

--- contracts/interfaces/IAxelarGMPTransfer.sol
+++ contracts/interfaces/IAxelarGMPTransfer.sol
@@ -20,6 +20,7 @@ interface IInterchainTransferSent {
     event InterchainTransferSent(
         string destinationChain,
         string destinationContractAddress,
+        address indexed sender,
         bytes recipient,
         address indexed token,
         uint256 amount

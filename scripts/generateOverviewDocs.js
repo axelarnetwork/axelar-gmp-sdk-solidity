87671c5a769b92ba525beb14706890396688ab6d
TanvirDeol <tanvir@interoplabs.io>
2025-02-11 13:48:15 -0500

    stop appending to string

--- scripts/generateOverviewDocs.js
+++ scripts/generateOverviewDocs.js
@@ -4,16 +4,16 @@ const fs = require('fs');
 const path = require('path');
 
 function generateIndexMarkdown(dir, outputFile, topLevelDir = 'docs') {
-    let content = '# Docs\n\n';
+    const content = ['# Docs\n\n'];
 
     // List all Markdown files in the directory under "Contracts"
     const files = fs.readdirSync(dir).filter((file) => file.endsWith('.md') && file !== 'index.md');
 
     if (files.length > 0) {
-        content += '## Contracts\n';
+        content.push('## Contracts\n');
         files.forEach((file) => {
             const title = path.parse(file).name;
-            content += `- [${title}](${file})\n`;
+            content.push(`- [${title}](${file})\n`);
         });
     }
 
@@ -21,12 +21,12 @@ function generateIndexMarkdown(dir, outputFile, topLevelDir = 'docs') {
     const subdirs = fs.readdirSync(dir).filter((subdir) => fs.statSync(path.join(dir, subdir)).isDirectory());
 
     if (subdirs.length > 0) {
-        content += '\n## Directories\n';
+        content.push('\n## Directories\n');
         subdirs.forEach((subdir) => {
             const childDir = path.join(dir, subdir);
             const childOutput = path.join(childDir, 'index.md');
             generateIndexMarkdown(childDir, childOutput, topLevelDir);
-            content += `- [${subdir}](${subdir}/index.md)\n`;
+            content.push(`- [${subdir}](${subdir}/index.md)\n`);
         });
     }
 
@@ -35,10 +35,10 @@ function generateIndexMarkdown(dir, outputFile, topLevelDir = 'docs') {
 
     if (fs.existsSync(readmePath) && dir === topLevelDir) {
         const readmeContent = fs.readFileSync(readmePath, 'utf-8');
-        content += `\n\n${readmeContent}`;
+        content.push(`\n\n${readmeContent}`);
     }
 
-    fs.writeFileSync(outputFile, content);
+    fs.writeFileSync(outputFile, content.join(''));
     console.log(`Generated ${outputFile}`);
 }
 

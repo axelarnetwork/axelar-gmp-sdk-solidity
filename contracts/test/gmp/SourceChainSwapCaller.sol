fa237515e108a4dc744d6b4657e2d811073f5824
Hannes Verschore <hv1989@gmail.com>
2026-04-01 08:47:11 +0200

    refactor: remove the deprecated sendToken (#220)

--- contracts/test/gmp/SourceChainSwapCaller.sol
+++ contracts/test/gmp/SourceChainSwapCaller.sol
@@ -2,11 +2,11 @@
 
 pragma solidity ^0.8.0;
 
-import { IAxelarGatewayWithToken } from '../../interfaces/IAxelarGatewayWithToken.sol';
+import { AxelarExecutableWithToken } from '../../executable/AxelarExecutableWithToken.sol';
 import { IERC20 } from '../../interfaces/IERC20.sol';
+import { StringToAddress } from '../../libs/AddressString.sol';
 
-contract SourceChainSwapCaller {
-    IAxelarGatewayWithToken public gateway;
+contract SourceChainSwapCaller is AxelarExecutableWithToken {
     string public destinationChain;
     string public executableAddress;
 
@@ -14,8 +14,7 @@ contract SourceChainSwapCaller {
         address gateway_,
         string memory destinationChain_,
         string memory executableAddress_
-    ) {
-        gateway = IAxelarGatewayWithToken(gateway_);
+    ) AxelarExecutableWithToken(gateway_) {
         destinationChain = destinationChain_;
         executableAddress = executableAddress_;
     }
@@ -26,12 +25,34 @@ contract SourceChainSwapCaller {
         uint256 amount,
         string memory recipient
     ) external payable {
-        address tokenX = gateway.tokenAddresses(symbolA);
+        address tokenX = gatewayWithToken().tokenAddresses(symbolA);
         bytes memory payload = abi.encode(symbolB, recipient);
 
         IERC20(tokenX).transferFrom(msg.sender, address(this), amount);
 
-        IERC20(tokenX).approve(address(gateway), amount);
-        gateway.callContractWithToken(destinationChain, executableAddress, payload, symbolA, amount);
+        IERC20(tokenX).approve(address(gateway()), amount);
+        gatewayWithToken().callContractWithToken(destinationChain, executableAddress, payload, symbolA, amount);
+    }
+
+    function _executeWithToken(
+        bytes32, /*commandId*/
+        string calldata, /*sourceChain*/
+        string calldata, /*sourceAddress*/
+        bytes calldata payload,
+        string calldata tokenSymbolB,
+        uint256 amount
+    ) internal override {
+        string memory recipientStr = abi.decode(payload, (string));
+        address tokenB = gatewayWithToken().tokenAddresses(tokenSymbolB);
+        IERC20(tokenB).transfer(StringToAddress.toAddress(recipientStr), amount);
+    }
+
+    function _execute(
+        bytes32, /*commandId*/
+        string calldata, /*sourceChain*/
+        string calldata, /*sourceAddress*/
+        bytes calldata /*payload*/
+    ) internal pure override {
+        revert('Not implemented');
     }
 }
